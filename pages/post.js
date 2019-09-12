e59b021ddc9c8af88776dfa17135115b4f266a02
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-12 17:13:53 +0100

    passing props to components

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,27 @@
-const Post = ({id}) => {
-    return (
+import axios from 'axios';
+
+const Post = ({id, comments }) => (
         <>
-        <h1>Post Page</h1>  
-        <p>{id}</p>
+        <h1>Comments from Post #{id}</h1>  
+            {comments.map(comment => (
+                <Comment { ...comment } /> 
+            ))}
         </>
-    );   
-}
+);
+
+const Comment = ({ email, body }) => (
+    <div>
+        <p>{body}</p>
+        <p>{email}</p>
+</div> 
+)
+    
 
 Post.getInitialProps = async ({query}) => { 
-    return query;
+    const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`);
+    const { data } = res;
+    return {...query, comments: data };
 };
 
 
