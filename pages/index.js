811ce82ceea830b6c979afd382175bd411f7a26c
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-12 15:56:56 +0100

    using getInitialProps to allow server side and client side rendering depending on routing

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
-const Index = () => {
-    console.log("index finger?");
-    return (
-        <div>
-           <h1>Home</h1>
-        </div>
-    );
-}
+const Index = ({posts}) => {
+        return (
+            <div>
+                <h1>Home</h1>
+                <h2>Rendering</h2>
+                <ul>
+                    {posts.map(post => (
+                         <li key={post.id}>{post.title}</li>
+
+                    ))}
+                </ul>
+            </div>
+        );
+};
+
+Index.getInitialProps = async () => { 
+    const res = await axios.get("https://jsonplaceholder.typicode.com/albums");
+    const { data } = res;
+    return { posts: data };
+};
+
 export default Index;
