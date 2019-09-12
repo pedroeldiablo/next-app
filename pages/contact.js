84490beb7e499ce0fc4a3a8b0a018e2462b9d8a2
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-12 12:58:59 +0100

    Initial set-up Next.js app

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function contact() {
+    console.log("where am i running?");
+    return (
+        <div>
+            <h1>Leave me alone</h1>
+        </div>
+    )
+}
